4c96bae82e7ccc37864c56ed9cd2148be264392b
iahearn001 <iahearn001@student.franciscan.edu>
2024-01-25 11:10:48 -0500

    Completed m1

--- src/m1_string_slice_and_concat.py
+++ src/m1_string_slice_and_concat.py
@@ -1,7 +1,7 @@
 txt = "  The quick brown fox jumps over the lazy dog.  "
 
 ###############################################################################
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #   Notice the string   txt   that I have defined above. First, let's clear up
 #   those extra spaces.
 #
@@ -16,7 +16,9 @@ txt = "  The quick brown fox jumps over the lazy dog.  "
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
-
+print(txt)
+txt = txt.strip()
+print(txt)
 ###############################################################################
 # TODO: 2. (1 pt)
 #   From now on, do not reassign the variable   txt   . You will be using it as
